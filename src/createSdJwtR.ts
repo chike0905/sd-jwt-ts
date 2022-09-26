436213a8e6154bfed0d56ea355ca9c980ae23007
Ryosuke Abe <chike@sfc.wide.ad.jp>
2022-09-26 16:02:22 +0900

    fix: create SD-JWT-R  in JWT format

--- src/createSdJwtR.ts
+++ src/createSdJwtR.ts
@@ -1,27 +1,40 @@
-import { base64url } from "jose";
+import { base64url, CompactSign, KeyLike, SignJWT } from "jose";
 import { SVC, SD_JWT_RELEASE } from "./types";
 import { separateJWTandSVC } from "./utils";
 
-export const createSDJWTRelease = (svc: SVC, discloseClaims: string[]): SD_JWT_RELEASE => {
-  let sd_jwt_release: SD_JWT_RELEASE = {
+export const createSDJWTRelease = async (
+  svc: SVC,
+  discloseClaims: string[],
+  privateKey: KeyLike
+): Promise<string> => {
+  let payload: SD_JWT_RELEASE = {
     sd_release: {}
   };
 
   discloseClaims.map((item) => {
-    Object.defineProperty(sd_jwt_release.sd_release, item, {
+    Object.defineProperty(payload.sd_release, item, {
       value: svc.sd_release[item],
       enumerable: true,
     });
   });
 
-  return sd_jwt_release;
+  // NOTE: tmp SD-JWT-R is JWT (JWS that has encoded json as the payload)
+  const sdJwtRelease = await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'ES256' })
+    .sign(privateKey);
+
+  return sdJwtRelease;
 }
 
-export const createSDJWTwithRelease = (sdJwt: string, discloseClaims: string[]): string => {
+export const createSDJWTwithRelease = async (
+  sdJwt: string,
+  discloseClaims: string[],
+  privateKey: KeyLike
+): Promise<string> => {
   const { svc, jwt } = separateJWTandSVC(sdJwt);
 
-  const release = createSDJWTRelease(svc, discloseClaims);
-  const encodedRelease = base64url.encode(JSON.stringify(release));
+  const release = await createSDJWTRelease(svc, discloseClaims, privateKey);
+  // const encodedRelease = base64url.encode(JSON.stringify(release));
 
-  return jwt + '.' + encodedRelease;
+  return jwt + '.' + release;
 }
--- tests/createSdJwtR.test.ts
+++ tests/createSdJwtR.test.ts
@@ -1,49 +1,63 @@
-import { base64url, decodeJwt } from "jose";
+import { base64url, compactVerify, decodeJwt, importJWK, jwtVerify, KeyLike } from "jose";
 import * as crypto from 'crypto';
 
-import { createSVCandSDDigests, SD_DIGESTS } from "../src";
+import { createSVCandSDDigests, SD_DIGESTS, SD_JWT_RELEASE } from "../src";
 import { createSDJWTRelease, createSDJWTwithRelease } from "../src/createSdJwtR";
 
-import { PAYLOAD, SAMPLE_SD_JWT } from "./params";
+import { PAYLOAD, PRIVATE_KEY_JWK, PUBLIC_KEY_JWK, SAMPLE_SD_JWT } from "./params";
 
 // 5.6 SD-JWT Release
 // For each claim, an array of the salt and the claim value is contained in the sd_release object. The structure of sd_release object in the SD-JWT-R is the same as in SD-JWT.
-it('Create SD-JWT Release', () => {
+it('Create SD-JWT Release', async () => {
+  const privKey = await importJWK(PRIVATE_KEY_JWK, 'ES256') as KeyLike;
   const { svc } = createSVCandSDDigests(PAYLOAD);
   const discloseClaims = ['given_name', 'family_name'];
-  const sd_jwt_release = createSDJWTRelease(svc, discloseClaims);
-  expect(sd_jwt_release).toHaveProperty('sd_release');
+
+  const sdJwtRelease: string = await createSDJWTRelease(svc, discloseClaims, privKey);
+
+  // const splittedSdJwtR = sdJwtRelease.split('.');
+  // expect(splittedSdJwtR.length).toBe(3);
+
+  const pubkey = await importJWK(PUBLIC_KEY_JWK, 'ES256') as KeyLike;
+  // NOTE: tmp SD-JWT-R is JWT (JWS that has encoded json as the payload)
+  const { payload, protectedHeader } = await jwtVerify(sdJwtRelease, pubkey);
+
+  expect(payload).toHaveProperty('sd_release');
   discloseClaims.map((item) => {
-    expect(sd_jwt_release.sd_release).toHaveProperty(item);
-    expect(sd_jwt_release.sd_release[item]).toBe(svc.sd_release[item]);
+    expect(payload.sd_release).toHaveProperty(item);
+    // @ts-ignore
+    expect(payload.sd_release[item]).toBe(svc.sd_release[item]);
   });
 });
 
 // TODO: Holder Binding
-// TODO: missing MUST specification bellow
 // 5.6
 // When the holder sends the SD-JWT-R to the Verifier, the SD-JWT-R MUST be a JWS represented as the JWS Compact Serialization as described in Section 7.1 of [RFC7515].
-it('Create SD-JWT with Release', () => {
+it('Create SD-JWT with Release', async () => {
+  const privKey = await importJWK(PRIVATE_KEY_JWK, 'ES256') as KeyLike;
   const discloseClaims = ['given_name', 'family_name'];
-  const sdJwtWithRelease: string = createSDJWTwithRelease(SAMPLE_SD_JWT, discloseClaims);
+  const sdJwtWithRelease: string =
+    await createSDJWTwithRelease(SAMPLE_SD_JWT, discloseClaims, privKey);
 
   // SD-JWT is combined jwt and sd-jwt-r;
   const splittedSdJwt = sdJwtWithRelease.split('.');
-  expect(splittedSdJwt.length).toBe(4);
+  expect(splittedSdJwt.length).toBe(6); // SD-JWT + SD-JWT-R as JWT
 
-  const rawRelease = base64url.decode(splittedSdJwt[3]).toString();
-  const release = JSON.parse(rawRelease);
-  const jwt = splittedSdJwt.slice(0, 3).join('.');
-  const payload = decodeJwt(jwt);
+  const sdJwt = splittedSdJwt.slice(0, 3).join('.');
+  const sdJwtR = splittedSdJwt.slice(3).join('.');
 
-  expect(release).toHaveProperty('sd_release');
+  const sdJwtPayload = decodeJwt(sdJwt);
+  const sdJwtRPayload = decodeJwt(sdJwtR) as SD_JWT_RELEASE;
+
+  expect(sdJwtRPayload).toHaveProperty('sd_release');
   discloseClaims.map((item) => {
-    expect(release.sd_release).toHaveProperty(item);
+    expect(sdJwtRPayload.sd_release).toHaveProperty(item);
   });
 
   discloseClaims.map((key) => {
+    // @ts-ignore
     const hashOfValueInRelease_b64 = base64url.encode(crypto.createHash('sha256')
-      .update(release.sd_release[key]).digest());
-    expect((payload.sd_digests as SD_DIGESTS)[key]).toBe(hashOfValueInRelease_b64);
+      .update(sdJwtRPayload.sd_release[key]).digest());
+    expect((sdJwtPayload.sd_digests as SD_DIGESTS)[key]).toBe(hashOfValueInRelease_b64);
   });
 });
