514abe5d99682fa7bcdaabd53b08f20a893a1553
Ryosuke Abe <chike@sfc.wide.ad.jp>
2022-09-28 10:47:26 +0900

    Implement holder binding in createSdJwtR

--- src/createSdJwtR.ts
+++ src/createSdJwtR.ts
@@ -1,11 +1,12 @@
-import { base64url, CompactSign, KeyLike, SignJWT } from "jose";
+import { base64url, CompactSign, decodeJwt, importJWK, JWK, jwtVerify, KeyLike, SignJWT, UnsecuredJWT } from "jose";
+import * as crypto from 'crypto';
 import { SVC, SD_JWT_RELEASE } from "./types";
 import { separateJWTandSVC } from "./utils";
 
 export const createSDJWTRelease = async (
   svc: SVC,
   discloseClaims: string[],
-  privateKey: KeyLike
+  privateKey?: KeyLike
 ): Promise<string> => {
   let payload: SD_JWT_RELEASE = {
     sd_release: {}
@@ -19,9 +20,14 @@ export const createSDJWTRelease = async (
   });
 
   // NOTE: tmp SD-JWT-R is JWT (JWS that has encoded json as the payload)
-  const sdJwtRelease = await new SignJWT(payload)
-    .setProtectedHeader({ alg: 'ES256' })
-    .sign(privateKey);
+  let sdJwtRelease: string
+  if (privateKey) {
+    sdJwtRelease = await new SignJWT(payload)
+      .setProtectedHeader({ alg: 'ES256' })
+      .sign(privateKey);
+  } else {
+    sdJwtRelease = new UnsecuredJWT(payload).encode();
+  }
 
   return sdJwtRelease;
 }
@@ -29,12 +35,26 @@ export const createSDJWTRelease = async (
 export const createSDJWTwithRelease = async (
   sdJwt: string,
   discloseClaims: string[],
-  privateKey: KeyLike
+  HolderPrivateKey?: KeyLike
 ): Promise<string> => {
   const { svc, jwt } = separateJWTandSVC(sdJwt);
 
-  const release = await createSDJWTRelease(svc, discloseClaims, privateKey);
+  const release = await createSDJWTRelease(svc, discloseClaims, HolderPrivateKey);
   // const encodedRelease = base64url.encode(JSON.stringify(release));
+
+  // NOTE: Temporary implementation for holder binding 
+  // if SD-JWT includes sub_jwk, only the key specified in sub_jwk can create signed SD-JWT-R. 
+  // It is able to create unsigned SD-JWT-R even if the SD-JWT includes sub_jwk. 
+  const jwtPayload = decodeJwt(jwt);
+  let boundedKey: KeyLike;
+  if (jwtPayload.hasOwnProperty('sub_jwk') && HolderPrivateKey) {
+    boundedKey = await importJWK(jwtPayload.sub_jwk as JWK, 'ES256') as KeyLike;
+    try {
+      await jwtVerify(release, boundedKey);
+    } catch (e) {
+      throw new Error('Public key of the specified private key is not bounded to the SD-JWT.');
+    }
+  }
 
   return jwt + '.' + release;
 }
--- tests/createSdJwtR.test.ts
+++ tests/createSdJwtR.test.ts
@@ -1,4 +1,4 @@
-import { base64url, decodeJwt, jwtVerify } from "jose";
+import { base64url, decodeJwt, jwtVerify, UnsecuredJWT } from "jose";
 import * as crypto from 'crypto';
 
 import { PAYLOAD, importKeyPairForIssuerAndHolder, Entity } from "./params";
@@ -16,7 +16,7 @@ let TEST_SVC: SVC;
 
 beforeEach(async () => {
   ({ ISSUER, HOLDER } = await importKeyPairForIssuerAndHolder());
-  TEST_SD_JWT = await issueSDJWT(PAYLOAD, ISSUER.PRIVATE_KEY);
+  TEST_SD_JWT = await issueSDJWT(PAYLOAD, ISSUER.PRIVATE_KEY, HOLDER.PUBLIC_KEY);
   TEST_SVC = JSON.parse(base64url.decode(TEST_SD_JWT.split('.')[3]).toString());
 });
 
@@ -25,11 +25,24 @@ beforeEach(async () => {
 it('Create SD-JWT Release', async () => {
   const discloseClaims = ['given_name', 'family_name'];
 
-  const sdJwtRelease: string = await createSDJWTRelease(TEST_SVC, discloseClaims, ISSUER.PRIVATE_KEY);
+  const sdJwtRelease: string = await createSDJWTRelease(TEST_SVC, discloseClaims, HOLDER.PRIVATE_KEY);
 
   // NOTE: tmp SD-JWT-R is JWT (JWS that has encoded json as the payload)
-  const { payload } = await jwtVerify(sdJwtRelease, ISSUER.PUBLIC_KEY);
+  const { payload } = await jwtVerify(sdJwtRelease, HOLDER.PUBLIC_KEY);
 
+  expect(payload).toHaveProperty('sd_release');
+  discloseClaims.map((item) => {
+    expect(payload.sd_release).toHaveProperty(item);
+    // @ts-ignore
+    expect(payload.sd_release[item]).toBe(TEST_SVC.sd_release[item]);
+  });
+});
+
+it('Create SD-JWT Release without Signature', async () => {
+  const discloseClaims = ['given_name', 'family_name'];
+  const sdJwtRelease: string = await createSDJWTRelease(TEST_SVC, discloseClaims);
+
+  const { payload } = UnsecuredJWT.decode(sdJwtRelease);
   expect(payload).toHaveProperty('sd_release');
   discloseClaims.map((item) => {
     expect(payload.sd_release).toHaveProperty(item);
@@ -41,20 +54,28 @@ it('Create SD-JWT Release', async () => {
 // TODO: Holder Binding
 // 5.6
 // When the holder sends the SD-JWT-R to the Verifier, the SD-JWT-R MUST be a JWS represented as the JWS Compact Serialization as described in Section 7.1 of [RFC7515].
-it('Create SD-JWT with Release', async () => {
-  const discloseClaims = ['given_name', 'family_name'];
-  const sdJwtWithRelease: string =
-    await createSDJWTwithRelease(TEST_SD_JWT, discloseClaims, ISSUER.PRIVATE_KEY);
 
+const validateSdJWTwithRelease = async (
+  sdJwtWithRelease: string,
+  discloseClaims: string[],
+  holderBinding: boolean = true
+) => {
   // SD-JWT is combined jwt and sd-jwt-r;
   const splittedSdJwt = sdJwtWithRelease.split('.');
   expect(splittedSdJwt.length).toBe(6); // SD-JWT + SD-JWT-R as JWT
+  if (!holderBinding)
+    expect(splittedSdJwt[5]).toBe('');
 
   const sdJwt = splittedSdJwt.slice(0, 3).join('.');
   const sdJwtR = splittedSdJwt.slice(3).join('.');
 
-  const sdJwtPayload = decodeJwt(sdJwt);
-  const sdJwtRPayload = decodeJwt(sdJwtR) as SD_JWT_RELEASE;
+  const sdJwtPayload = (await jwtVerify(sdJwt, ISSUER.PUBLIC_KEY)).payload;
+  let sdJwtRPayload: SD_JWT_RELEASE;
+  if (holderBinding) {
+    sdJwtRPayload = (await jwtVerify(sdJwtR, HOLDER.PUBLIC_KEY)).payload as SD_JWT_RELEASE;
+  } else {
+    sdJwtRPayload = UnsecuredJWT.decode(sdJwtR).payload as SD_JWT_RELEASE;
+  }
 
   expect(sdJwtRPayload).toHaveProperty('sd_release');
   discloseClaims.map((item) => {
@@ -67,4 +88,48 @@ it('Create SD-JWT with Release', async () => {
       .update(sdJwtRPayload.sd_release[key]).digest());
     expect((sdJwtPayload.sd_digests as SD_DIGESTS)[key]).toBe(hashOfValueInRelease_b64);
   });
+};
+
+it('Create SD-JWT with Release', async () => {
+  const discloseClaims = ['given_name', 'family_name'];
+  const sdJwtWithRelease: string =
+    await createSDJWTwithRelease(TEST_SD_JWT, discloseClaims, HOLDER.PRIVATE_KEY);
+
+  await validateSdJWTwithRelease(sdJwtWithRelease, discloseClaims);
+});
+
+it('Create SD-JWT with Release without Holder binding', async () => {
+  const discloseClaims = ['given_name', 'family_name'];
+  const sdJwtWithRelease: string =
+    await createSDJWTwithRelease(TEST_SD_JWT, discloseClaims);
+
+  await validateSdJWTwithRelease(sdJwtWithRelease, discloseClaims, false);
+});
+
+it('Try to Create SD-JWT with Release by not bounded key', async () => {
+  const discloseClaims = ['given_name', 'family_name'];
+
+  await expect(createSDJWTwithRelease(TEST_SD_JWT, discloseClaims, ISSUER.PRIVATE_KEY))
+    .rejects.toThrow(
+      new Error('Public key of the specified private key is not bounded to the SD-JWT.')
+    );
+});
+
+it('Create not-holder-bounded SD-JWT with Release by a private key', async () => {
+  const discloseClaims = ['given_name', 'family_name'];
+  TEST_SD_JWT = await issueSDJWT(PAYLOAD, ISSUER.PRIVATE_KEY);
+  const sdJwtWithRelease: string =
+    await createSDJWTwithRelease(TEST_SD_JWT, discloseClaims, HOLDER.PRIVATE_KEY);
+
+  await validateSdJWTwithRelease(sdJwtWithRelease, discloseClaims);
+});
+
+it('Create not-holder-bounded SD-JWT with Release without private key', async () => {
+  const discloseClaims = ['given_name', 'family_name'];
+
+  TEST_SD_JWT = await issueSDJWT(PAYLOAD, ISSUER.PRIVATE_KEY);
+  const sdJwtWithRelease: string
+    = await createSDJWTwithRelease(TEST_SD_JWT, discloseClaims);
+
+  await validateSdJWTwithRelease(sdJwtWithRelease, discloseClaims, false);
 });
